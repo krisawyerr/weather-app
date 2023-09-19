53c525e262aeae9460f408081951ec684e054df9
Kris Sawyerr <krisawyerr@gmail.com>
2023-09-18 17:36:27 -0700

    made stats look nice

--- index.js
+++ index.js
@@ -40,28 +40,63 @@ function logValue(event) {
                 windDesc = 'it is advised to stay indoors'
             }
 
-           
 
-            document.querySelector(".wordTemp").innerHTML = 'temp:';
-            document.querySelector(".wordTempMin").innerHTML = 'temp min:';
-            document.querySelector(".wordTempMax").innerHTML = 'temp max:';
-            document.querySelector(".wordWindSpeed").innerHTML = 'wind speed:';
-            document.querySelector(".wordLat").innerHTML = 'lat:';
-            document.querySelector(".wordLong").innerHTML = 'long:';
-            document.querySelector(".wordDesc").innerHTML = 'description:';
-            document.querySelector(".wordHumi").innerHTML = 'humidity:';
+            document.querySelector(".body").innerHTML = `
+            <!-- Weather Stats -->
+            <div class="weatherStats" style="box-shadow: 0px 0px 10px #001f54;">
+                <div class="mainTemp">
+                    <p class="temp"></p>
+                </div>
+                <div class="altTemp">
+                    <div class="tempRow">
+                        <h3 class="wordFeelsLike"></h3>
+                        <p class="feelsLike tempRowTemp"></p>
+                    </div>
+                    <div class="tempRow">
+                        <h3 class="wordTempMin"></h3>
+                        <p class="tempMin tempRowTemp"></p>
+                    </div>
+                    <div class="tempRow">
+                        <h3 class="wordTempMax"></h3>
+                        <p class="tempMax tempRowTemp"></p>
+                    </div>
+                </div>
+                <div class="location">
+                    <img src="icons8-compass-50.png" alt="" width="30">
+                    <p class="latLong"></p>
+                </div>
+                <div class="mainWind">
+                    <img src="icons8-wind-50.png" alt="">
+                    <p class="windSpeed"></p>
+                </div>
+                <div class="mainHumid">
+                    <img src="icons8-water-drop-50.png" alt="">
+                    <p class="humi"></p>
+                </div>
+                
+            </div>
+            <!-- Maps -->
+            <div class="mapSection" style="box-shadow: 0px 0px 10px #001f54;">
+                <div class="mappy"></div>
+            </div> 
+            <!-- Weather Desc -->
+            <div class="weatherDesc" style="box-shadow: 0px 0px 10px #001f54;">
+                <p class="desc"></p>
+            </div>  `;
+
+            document.querySelector(".wordFeelsLike").innerHTML = 'Feels Like:';
+            document.querySelector(".wordTempMin").innerHTML = 'Daily Low:';
+            document.querySelector(".wordTempMax").innerHTML = 'Daily High:';
 
 
-            document.querySelector(".temp").innerHTML = data.main.temp;
-            document.querySelector(".humi").innerHTML = data.main.humidity;
-            document.querySelector(".long").innerHTML = data.coord.lon;
-            document.querySelector(".lat").innerHTML = data.coord.lat;
-            document.querySelector(".feelsLike").innerHTML = data.main.feels_like;
-            document.querySelector(".wordFeelsLike").innerHTML = 'feels like:';
-            document.querySelector(".tempMin").innerHTML = data.main.temp_min;
-            document.querySelector(".tempMax").innerHTML = data.main.temp_max;
-            document.querySelector(".windSpeed").innerHTML = data.wind.speed;
-            document.querySelector(".desc").innerHTML = `${data.name} is currently ${data.main.temp}°F, but it feels like ${data.main.feels_like}°F. Today is going to give you a high of ${data.main.temp_max}°F and a low of ${data.main.temp_min}°F. With a humidity of ${data.main.humidity}%, be ready to ${humDesc}. The wind speed is ${data.wind.speed}, so ${windDesc}.`;
+            document.querySelector(".temp").innerHTML = `${Math.round(data.main.temp)}°F`;
+            document.querySelector(".humi").innerHTML = `${data.main.humidity}%`;
+            document.querySelector(".latLong").innerHTML = `${data.coord.lat}, ${data.coord.lon}`;
+            document.querySelector(".feelsLike").innerHTML = `${Math.round(data.main.feels_like)}°F`;
+            document.querySelector(".tempMin").innerHTML = `${Math.round(data.main.temp_min)}°F`;
+            document.querySelector(".tempMax").innerHTML = `${Math.round(data.main.temp_max)}°F`;
+            document.querySelector(".windSpeed").innerHTML = `${Math.round(data.wind.speed)} mph`;
+            document.querySelector(".desc").innerHTML = `${data.name} is currently ${Math.round(data.main.temp)}°F, but it feels like ${Math.round(data.main.feels_like)}°F. Today is going to give you a high of ${data.main.temp_max}°F and a low of ${Math.round(data.main.temp_min)}°F. With a humidity of ${data.main.humidity}%, be ready to ${humDesc}. The wind speed is ${Math.round(data.wind.speed)} mph, so ${windDesc}.`;
             document.querySelector(".city").innerHTML = `${data.name}, ${data.sys.country}`;
             document.querySelector(".mappy").innerHTML = `<iframe width='1200px' height='400px' src="https://api.mapbox.com/styles/v1/krisawyerr/clmodveur01vt01ragti210rz.html?title=false&access_token=${mapKey}&zoomwheel=false#11/${mapLat}/${mapLon}" title="Satellite Streets" style="border:none;"></iframe>`
         }
